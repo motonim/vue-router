644b01a174085286aa1e90eb69579196ba2ea4f8
Jaeri <motonimjrp@gmail.com>
2024-01-17 11:50:34 -0500

    set route parameters

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-
+import Jobs from '../views/jobs/Jobs.vue';
+import JobDetails from '../views/jobs/JobDetails.vue';
 
 const routes = [
   {
@@ -13,6 +14,16 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path:'/jobs/:id',
+    name: 'jobDetails',
+    component: JobDetails
   }
 ]
 
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }}</h2>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                { title: 'UX designer', id: 1, details: 'lorem'},
+                { title: 'Web developer', id: 2, details: 'lorem 14123'},
+                { title: 'DevOps engineer', id: 3, details: 'lorem ipsum 23234234'},
+            ]
+        }
+    }
+}
+</script>
